c265f9d071a7593e4539861ea8553f9cb08b549f
donguri-py <turntabroz@gmail.com>
2023-01-09 14:43:14 +0900

    typeの練習

---
+++ type.py
@@ -0,0 +1,20 @@
+a = 123
+print(type(a))
+
+b = 123.456
+print(type(b))
+
+c = "Hello World"
+print(c)
+
+apple = 321
+
+pineapple = 654.321
+
+pen = "I have a pen."
+
+print(type(apple))
+
+print(type(pineapple))
+
+print(type(pen))
