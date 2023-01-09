0950f75eae341785dba0055a071644766c31226b
donguri-py <turntabroz@gmail.com>
2023-01-09 14:29:17 +0900

    functions

---
+++ q13.py
@@ -0,0 +1,23 @@
+x = divmod(17,3)
+
+print(x)
+
+print(5 ** 2)
+
+x = abs(-1)
+print(x)
+
+x = bin(100)
+print(x)
+
+x = oct(100)
+print(x)
+
+x = hex(100)
+print(x)
+
+x = 0b100
+print(x)
+
+x = 0o100
+print(x)
