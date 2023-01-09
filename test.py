7af9ef01f1371b56e9e426d3b6e0b001d0f7d3f9
donguri-py <turntabroz@gmail.com>
2023-01-09 14:40:08 +0900

    udemy test

---
+++ test.py
@@ -0,0 +1,22 @@
+a = 123
+print(a)
+
+a = 456
+print(a)
+
+b = 123.456
+c = "Hello World"
+
+pico0123 = 3210
+print(pico0123)
+
+i_have_a_pen = 123
+
+pen = 123
+print(pen)
+
+apple = 456
+print(apple)
+
+pineapple = 789
+print(pineapple)
