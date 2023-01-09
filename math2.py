99a02bae0363627e037b3b162106d36d1f95138e
donguri-py <turntabroz@gmail.com>
2023-01-09 16:23:39 +0900

    演算子

---
+++ math2.py
@@ -0,0 +1,23 @@
+j = 15
+k = 7
+
+a = j + k
+print(a)
+
+b = j - k
+print(b)
+
+c = j * k
+print(c)
+
+d = j / k
+print(d)
+
+e = j // k
+print(e)
+
+f = j % k
+print(f)
+
+g = k ** 2
+print(g)
---
+++ str.py
@@ -0,0 +1,29 @@
+a = "Hello world"
+print(a)
+
+b = 'Hello world'
+print(b)
+
+c = 'Hello "Python" world!'
+print(c)
+
+d = "Hello 'Python' world!"
+print(d)
+
+e = """
+I have a pen.
+I have a apple.
+"""
+print(e)
+
+apple = "I have an apple."
+print(apple)
+
+pineapple = "I have a 'big' paineapple."
+print(pineapple)
+
+pen = """I have a pen.
+I have an apple.
+I have a pineapple
+"""
+print(pen)
