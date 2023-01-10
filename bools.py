581146425f0e8f4e201e1b00bbede4e4f12ea245
donguri-py <turntabroz@gmail.com>
2023-01-11 07:12:38 +0900

    space

---
+++ bools.py
@@ -0,0 +1,18 @@
+favorite = "like"
+fruit = "apple"
+
+x = "I {} {}".format(favorite,fruit)
+
+print(x)
+
+y = f"i {favorite} {fruit}"
+
+print(y)
+
+name = "ken"
+age = 12
+
+a = "{:10}".format(name)
+b = "{:10}".format(age)
+
+print(a, b)
